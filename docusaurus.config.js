d36743f26a4b3bf7d6dc620b6af6c2a5f19d9d90
Mario Frei <mario.frei@gmx.net>
2022-11-22 20:44:04 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Cozie, a Fitbit Clockface for IEQ and physiological data collection',
   tagline: 'Allows building occupants to provide feedback in real time',
-  url: 'https://cozie.app/',
-  baseUrl: '/',
+  url: 'https://cozie-app.github.io',
+  baseUrl: '/cozie-fitbit-website-new/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'cozie-app', // Usually your GitHub org/user name.
-  projectName: 'cozie', // Usually your repo name.
+  projectName: 'cozie-fitbit-website-new', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ThePebblesFr/cozie-website/tree/master/my-website',
+            'https://github.com/cozie-app/cozie-fitbit-website-new/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -63,7 +63,7 @@ const config = {
             label: 'Documentation',
           },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/cozie-app/cozie',
             label: 'GitHub',
             position: 'right',
           },
